0f7b2d746b21d671a5d9977190c69eee459f3cac
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2024-03-15 12:07:59 +0200

    fix spec

--- spec/iostruct_spec.rb
+++ spec/iostruct_spec.rb
@@ -3,20 +3,25 @@ require 'stringio'
 
 describe IOStruct do
   describe "#read" do
+    let(:a) { [12345, 56789] }
+    let(:data) { a.pack('L2') }
+
     it "reads from IO" do
-      a = [12345, 56789]
-      data = a.pack('L2')
       x = IOStruct.new('LL', :x, :y).read(StringIO.new(data))
       expect(x.x).to eq a[0]
       expect(x.y).to eq a[1]
     end
 
     it "reads from String" do
-      a = [12345, 56789]
-      data = a.pack('L2')
       x = IOStruct.new('LL', :x, :y).read(data)
       expect(x.x).to eq a[0]
       expect(x.y).to eq a[1]
+    end
+
+    it "creates a new instance of a subclass" do
+      klass = Class.new( IOStruct.new('LL', :x, :y) )
+      x = klass.read(data)
+      expect(x).to be_a klass
     end
   end
 
